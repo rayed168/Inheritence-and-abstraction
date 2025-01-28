b646960c97152a96e679eabb019f725a56f7a9cb
rayed168 <muhammad.rayed2013@gmail.com>
2025-01-28 17:00:00 +0200

    First commit

---
+++ Emplyee details.py
@@ -0,0 +1,21 @@
+class Employer:
+    def __init__(self, name, id_number):
+        self.name = name
+        self.id_number = id_number
+    def display(self):
+        print(self.name)
+        print(self.id_number)
+
+
+class Emplyee(Employer):
+    def __init__(self, name, id_number, salary, post):
+        self.salary = salary
+        self.post = post
+        Emplyee.__init__(self, name, id_number)
+name = input("Enter your name : ")
+id_number =int(input("Enter your id number : "))
+salary = int(input("Enter your salary : "))
+post = input("Enter your post : ")
+
+obj = Emplyee(name, id_number, salary, post)
+obj.display()
